b66b2a4de74c9805b06583b5474eb7b74c7bbd70
Julien Thomas <jthomas@zenetys.com>
2021-10-20 12:05:48 +0200

    FEATURE: entities: Implement /entities
    Return an array of string representing the available entities.

---
+++ handler_entities.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const config = require(__dirname + '/config.js');
+const Handler = require(__dirname + '/handler.js');
+const Logger = require(__dirname + '/logger.js');
+const util = require(__dirname + '/util.js');
+
+const OPTIONS = {
+    log: new Logger('entities'),
+    contentType: 'application/json',
+};
+
+class HandlerEntities extends Handler {
+
+    constructor() {
+        super(OPTIONS);
+    }
+
+    async process(ctx) {
+        var entities;
+
+        try {
+            entities = util.lsDirSync(config.options.dataDir, { lstat: true,
+                filter: (d,n,s) => s.isDirectory() && n.substr(0, 1) != '.' });
+        }
+        catch (e) {
+            this.log.error(e);
+            this.log.error('Failed to list entities');
+            ctx.res.writeHead(500);
+            return;
+        }
+
+        entities.sort();
+        ctx.res.writeHead(200);
+        ctx.res.end(JSON.stringify(entities));
+    }
+}
+
+module.exports = HandlerEntities;
