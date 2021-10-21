b93d59e7ee338bcfaefa3a260bae5ce5b2ec2f0f
Julien Thomas <jthomas@zenetys.com>
2021-10-21 22:43:32 +0200

    MINOR: handler/time: Accept a wait parameter from the query string
    This is a simple test case to await something.

--- handler_time.js
+++ handler_time.js
@@ -2,10 +2,19 @@
 
 const Handler = require(__dirname + '/handler.js');
 const Logger = require(__dirname + '/logger.js');
+const util = require(__dirname + '/util.js');
 
 const OPTIONS = {
     log: new Logger('time'),
     contentType: 'application/json',
+    inputRules: {
+        qs: {
+            type: 'object',
+            fields: {
+                wait: { type: 'number' }
+            }
+        }
+    },
 };
 
 class HandlerTime extends Handler {
@@ -15,6 +24,10 @@ class HandlerTime extends Handler {
     }
 
     async process(ctx) {
+        if (ctx.url.qs.wait) {
+            this.log.info(`Wait ${ctx.url.qs.wait} ms`);
+            await util.sleep(ctx.url.qs.wait);
+        }
         this.log.info('Reply our time to the client')
         var data = { time: (new Date()).getTime() / 1000 };
         ctx.res.writeHead(200);
