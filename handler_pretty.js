fabb94fb4a841b00db135215bb5e4ef662d91c92
Julien Thomas <jthomas@zenetys.com>
2021-10-22 10:12:43 +0200

    MINOR: handler/pretty: Add /pretty handler to help test JSON input data

---
+++ handler_pretty.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Handler = require(__dirname + '/handler.js');
+const Logger = require(__dirname + '/logger.js');
+const util = require(__dirname + '/util.js');
+
+const OPTIONS = {
+    log: new Logger('pretty'),
+    contentType: 'text/plain',
+    inputData: {
+        enabled: true,
+        type: 'application/json',
+    }
+};
+
+class HandlerPretty extends Handler {
+
+    constructor() {
+        super(OPTIONS);
+    }
+
+    async process(ctx) {
+        this.log.info('Pretty echo back')
+        ctx.res.writeHead(200);
+        ctx.res.end(JSON.stringify(ctx.data, null, 4));
+    }
+}
+
+module.exports = HandlerPretty;
